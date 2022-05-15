30d971b10a6aff7d8fda8bd164904a1a202b2ffa
egorgorban <noreply.egorgorban@inbox.ru>
2022-05-16 00:11:25 +0300

    + nonrec DFS + parse csv + vertices number

--- CPPGraphs/main.cpp
+++ CPPGraphs/main.cpp
@@ -7,12 +7,12 @@
 #include <sstream>
 #include <ctime>
 #include <map>
+#include <stack>
 
 /**
  * @namespace graph
  * @brief Graph Algorithms
  */
-
 namespace graph
 {
 
@@ -25,6 +25,8 @@ namespace graph
      */
     void addEdge(std::vector<std::vector<int>> *adj, int u, int v)
     {
+        // здесь "-1" везде, потому что нумерация вершин после препроцессинга от 1
+        // а это в свою очередь было нужно из-за того, что вектор по умолчанию забивается нулями
         (*adj)[u - 1].push_back(v - 1);
         (*adj)[v - 1].push_back(u - 1);
     }
@@ -37,15 +39,44 @@ namespace graph
      * @param u vertex or node to be explored.
      * @param visited already visited vertices.
      */
-    void explore(const std::vector<std::vector<int>> *adj, int u,
-                 std::vector<bool> *visited)
+    void DFS(const std::vector<std::vector<int>> *adj, int u, std::vector<bool> *visited)
     {
         (*visited)[u] = true;
         for (auto v : (*adj)[u])
         {
             if (!(*visited)[v])
             {
-                explore(adj, v, visited);
+                DFS(adj, v, visited);
+            }
+        }
+    }
+
+    /**
+     * @brief нерекурсивный дфс на стеке, изменяет visited на месте
+     *
+     * @param adj список смежности
+     * @param root вершина, от которой строим ДФС
+     * @param visited массив bool, где храним метки посещена/не посещена вершина
+     * @return ** void
+     */
+    void nonRecursiveDFS(const std::vector<std::vector<int>> *adj, int root, std::vector<bool> *visited)
+    {
+        (*visited)[root] = true;
+
+        std::stack<int> s;
+        s.push(root);
+
+        while (!s.empty())
+        {
+            int u = s.top();
+
+            (*visited)[u] = true;
+            s.pop();
+
+            for (auto v : (*adj)[u])
+            {
+                if (!(*visited)[v])
+                    s.push(v);
             }
         }
     }
@@ -58,7 +89,7 @@ namespace graph
      *
      * @return connected_components number of connected components in graph.
      */
-    int getConnectedComponents(const std::vector<std::vector<int>> *adj)
+    int getConnectedComponents(const std::vector<std::vector<int>> *adj, bool nonRecursive = false)
     {
         int n = adj->size();
         int connected_components = 0;
@@ -68,13 +99,76 @@ namespace graph
         {
             if (!visited[i])
             {
-                explore(adj, i, &visited);
+                if (nonRecursive)
+                    nonRecursiveDFS(adj, i, &visited);
+                else
+                    DFS(adj, i, &visited);
                 connected_components++;
             }
         }
         return connected_components;
     }
 } // namespace graph
+
+std::pair<int, int> parse_txt_line(std::string line)
+{
+    int u, v;
+    std::istringstream iss(line);
+
+    iss >> u;
+    iss >> v;
+
+    return std::make_pair(u, v);
+}
+
+std::pair<int, int> parse_csv_line(std::string line)
+{
+    int u, v;
+    char colon;
+    std::istringstream iss(line);
+
+    iss >> u;
+    iss >> colon;
+    iss >> v;
+
+    return std::make_pair(u, v);
+}
+
+/**
+ * @brief Get the Number Vertices of graph in file
+ *
+ * @param filename
+ * @return int
+ */
+int getNumberVertices(std::string filename)
+{
+    std::ifstream in(filename);
+    std::string line;
+    std::map<int, int> edges;
+
+    int maxNumber = 0;
+
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            std::pair<int, int> vertices;
+            vertices = parse_txt_line(line);
+
+            int newMax = std::max(vertices.first, vertices.second);
+
+            if (maxNumber < newMax)
+                maxNumber = newMax;
+        }
+        in.close();
+    }
+
+    return maxNumber;
+}
+bool getIsTxt(std::string filename)
+{
+    return filename.substr(filename.find_last_of(".") + 1) == "txt";
+}
 
 /**
  * @brief Просматривает строчки начинающиеся с цифры.
@@ -96,8 +190,9 @@ void preprocess(std::string filename, std::string outputFilename)
     std::ofstream out(outputFilename);
     std::string line;
 
+    bool isTxt = getIsTxt(filename);
+
     std::map<int, int> edges;
-    int u, v;
 
     int count_vertices = 0; // ! номер первой вершины будет равен 1, не 0 !
 
@@ -107,12 +202,14 @@ void preprocess(std::string filename, std::string outputFilename)
         {
             if (isdigit(line[0]))
             {
-                std::istringstream iss(line);
+                std::pair<int, int> vertices;
+                if (isTxt)
+                    vertices = parse_txt_line(line);
+                else
+                    vertices = parse_csv_line(line);
 
-                iss >> u;
-                iss >> v;
-
-                std::cout << u << v << std::endl;
+                int u = vertices.first;
+                int v = vertices.second;
 
                 if (edges[u] == 0)
                     edges[u] = ++count_vertices;
@@ -133,16 +230,30 @@ int main()
 
     clock_t t = clock();
 
-    std::string filename = "CA-AstroPh.txt";
-    int NUMBER_VERTICES = 18772;
+    std::string sourceFilename;
+    std::string filename;
+    int NUMBER_VERTICES;
 
-    preprocess(filename, "astro.txt");
+    sourceFilename = "local/CA-AstroPh.txt";
+    filename = "local/astro.txt";
+
+    // sourceFilename = "local/text.txt";
+    // filename = "local/a.txt";
+
+    // sourceFilename = "local/vk.csv";
+    // filename = "local/vk.txt";
+
+    preprocess(sourceFilename, filename);
 
     std::cout << "Время препроцессинга: " << (clock() - t) / 1e3 << " мс" << std::endl;
     t = clock();
 
-    std::ifstream in("a.txt");
+    std::ifstream in(filename);
     std::string line;
+
+    NUMBER_VERTICES = getNumberVertices(filename);
+
+    std::cout << "NUMBER_VERTICES: " << NUMBER_VERTICES << std::endl;
 
     std::vector<std::vector<int>> adj(NUMBER_VERTICES, std::vector<int>());
     int u, v;
@@ -162,7 +273,13 @@ int main()
 
     std::cout << "Время делания графа: " << (clock() - t) / 1e3 << " мс" << std::endl;
     t = clock();
-    int cc = graph::getConnectedComponents(&adj);
+
+    int cc;
+    bool isVk = filename.find("vk") != std::string::npos;
+    if (isVk)
+        cc = graph::getConnectedComponents(&adj, true);
+    else
+        cc = graph::getConnectedComponents(&adj, false);
 
     std::cout << "Время подсчёта компонент: " << (clock() - t) / 1e3 << " мс" << std::endl
               << std::endl;
