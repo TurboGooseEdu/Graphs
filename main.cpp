8970b6c204f94bd08d6b42c5129bc72d29264b0a
egorgorban <noreply.egorgorban@inbox.ru>
2022-05-21 23:02:01 +0300

    upd main

--- main.cpp
+++ main.cpp
@@ -1,47 +1,30 @@
 #include <iostream>
 #include <string>
-#include <ctime>
-#include "algorithms/graph.h"
+
 #include "algorithms/basicAlgorithms.h"
+#include "graphs/UndirectedGraph.h"
 
-/** Main function */
-int main()
-{
-    clock_t t = clock();
+int main() {
 
-    Graph *astroGraph = new Graph("data/CA-AstroPh.txt", "data/astro.txt");
+    UndirectedGraph *astroGraph = new UndirectedGraph("data/CA-AstroPh.txt", "data/astro.txt", 18772);
+    UndirectedGraph *vkGraph = new UndirectedGraph("data/vk.csv", "data/vk.txt", 3215722);
+    UndirectedGraph *googleGraph = new UndirectedGraph("data/web-Google.txt", "data/google.txt", 875713);
 
-    std::cout << "время обработки 1 " << (clock() - t) / 1e6 << " с." << std::endl;
-    t = clock();
-
-    Graph *vkGraph = new Graph("data/vk.csv", "data/vk.txt");
-    std::cout << "время обработки 2 " << (clock() - t) / 1e6 << " с." << std::endl;
-    t = clock();
-
-    Graph *googleGraph = new Graph("data/web-Google.txt", "data/google.txt");
-    std::cout << "время обработки 3 " << (clock() - t) / 1e6 << " с." << std::endl;
-    t = clock();
-
-    std::vector<Graph *> graphs{
+    std::vector<UndirectedGraph *> graphs{
         astroGraph,
         vkGraph,
         googleGraph,
     };
-    for (auto g : graphs)
-    {
+    for (auto g : graphs) {
         int cc = getConnectedComponents(*g);
 
         std::cout << "Количество компонент в графе " << g->getName() << ": " << cc << std::endl;
         std::cout << "Количество вершин: " << g->getVertices() << std::endl;
-
-        std::cout << "Время подсчёта: " << (clock() - t) / 1e6 << " с." << std::endl;
-        t = clock();
+        std::cout << "Количество рёбер: " << g->getEdges() << std::endl;
     }
 
     for (auto g : graphs)
-    {
         delete g;
-    }
 
     return 0;
 }
