f37be642d25402d478f53037808b96439b1de953
ilya <antonpuckich@gmail.com>
2022-05-24 22:54:22 +0300

    Implement task 1-1-2 (not tested yet)

--- tasks/task_1_1_1.py
+++ tasks/task_1_1_1.py
@@ -1,28 +1,82 @@
-from typing import Union, List, Set
+from math import inf
+from random import sample
+from typing import Union, List, Set, Tuple
 
 from graph_impls.directed_graph import DirectedGraph
 from graph_impls.undirected_graph import UndirectedGraph
 
 
-def calculate_task_1(graph: Union[DirectedGraph, UndirectedGraph]):
-    if type(graph) is UndirectedGraph:
-        print("Граф:", graph.name)
-        print("-" * 30)
-        print("Количество вершин:", graph.v)
-        print("Количество ребер:", graph.e)
-        print("Плотность графа:", calculate_density(graph))
-        wcc = get_weakly_connected_components(graph)
-        max_wcc = max(wcc, key=lambda x: len(x))
-        print("Число компонент слабой связности:", len(wcc))
-        print("Мощность максимальной компоненты слабой связности:", len(max_wcc))
-        print("Доля вершин в максимальной компоненте слабой связности:", len(max_wcc) / graph.v)
+def calculate_for_undirected(graph: UndirectedGraph):
+    print("Граф:", graph.name)
+    print("-" * 30)
+    print("Количество вершин:", graph.v)
+    print("Количество ребер:", graph.e)
+    print("Плотность графа:", calculate_density(graph))
+    wcc = get_weakly_connected_components(graph)
+    max_wcc = max(wcc, key=lambda x: len(x))
+    print("Число компонент слабой связности:", len(wcc))
+    print("Мощность максимальной компоненты слабой связности:", len(max_wcc))
+    print("Доля вершин в максимальной компоненте слабой связности:", len(max_wcc) / graph.v)
 
-    if type(graph) is DirectedGraph:
-        scc = get_strongly_connected_components(graph)
-        max_scc = max(scc, key=lambda x: len(x))
-        print("Число компонент сильной связности:", len(scc))
-        print("Мощность максимальной компоненты сильной связности:", len(max_scc))
-        print("Доля вершин в максимальной компоненте сильной связности:", len(max_scc) / graph.v)
+    wcc_part = choose_x_random_vertices(max_wcc, 500)
+    r, d, p = calculate_radius_diameter_percentile(graph, wcc_part)
+    print("Радиус наибольшей компоненты слабой связности:", r)
+    print("Диаметр наибольшей компоненты слабой связности:", d)
+    print("90 процентиль расстояния между вершинами графа:", p)
+
+
+def calculate_for_directed(undir_graph: UndirectedGraph, dir_graph: DirectedGraph):
+    calculate_for_undirected(undir_graph)
+    scc = get_strongly_connected_components(dir_graph)
+    max_scc = max(scc, key=lambda x: len(x))
+    print("Число компонент сильной связности:", len(scc))
+    print("Мощность максимальной компоненты сильной связности:", len(max_scc))
+    print("Доля вершин в максимальной компоненте сильной связности:", len(max_scc) / dir_graph.v)
+
+
+def calculate_radius_diameter_percentile(graph: UndirectedGraph, verts: Set[int], percentile=90) -> Tuple[int, int, int]:
+    min_ecc = inf  # radius
+    max_ecc = -1   # diameter
+    ranges = []
+
+    for i in range(len(verts) - 1):
+        ecc = -1
+        for j in range(i, len(verts)):
+            path_len = bfs(graph, i, j)
+            ranges.append(path_len)
+            if path_len > ecc:
+                ecc = path_len
+        if ecc < min_ecc:
+            min_ecc = ecc
+        if ecc > max_ecc:
+            max_ecc = ecc
+
+    ranges.sort()
+    p = ranges[percentile * len(ranges) // 100]
+    return min_ecc, max_ecc, p
+
+
+def bfs(graph: UndirectedGraph, start: int, goal: int) -> int:
+    if start == goal:
+        return 0
+    queue = [start]
+    visited = set()
+    visited.add(start)
+    level = 0
+    while queue:
+        s = queue.pop(0)
+        level += 1
+        for neighbour in graph.adj[s]:
+            if neighbour == goal:
+                return level
+            if neighbour not in visited:
+                visited.add(neighbour)
+                queue.append(neighbour)
+    return -1
+
+
+def choose_x_random_vertices(verts: Set[int], x: int) -> Set[int]:
+    return set(sample(list(verts), x))
 
 
 def calculate_density(graph: Union[DirectedGraph, UndirectedGraph]) -> int:
