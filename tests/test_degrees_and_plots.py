1ff9f1531108113c7adcd9244219836df3319257
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2022-05-25 15:59:29 +0300

    move the init function of a small graph to a separate file

--- tests/test_degrees_and_plots.py
+++ tests/test_degrees_and_plots.py
@@ -8,6 +8,7 @@ from tasks.degrees_and_plots import node_degrees, average_node_degree, average_n
 import numpy as np
 import matplotlib.pyplot as plt
 
+from utils.init_small_graph import init_graph_one
 from utils.random_graph import random_graph
 
 
@@ -30,35 +31,8 @@ def test_plots(array):
     plt.show()
 
 
-def init_graph(graph):
-    graph.add_edge(1, 5)
-    graph.add_edge(1, 8)
-    graph.add_edge(1, 7)
-    graph.add_edge(5, 8)
-    graph.add_edge(7, 8)
-    graph.add_edge(8, 4)
-    graph.add_edge(8, 3)
-    graph.add_edge(8, 6)
-    graph.add_edge(7, 2)
-    graph.add_edge(2, 6)
-    graph.add_edge(6, 3)
-    graph.add_edge(7, 5)
-    graph.add_edge(2, 8)
-    graph.add_edge(4, 7)
-    graph.add_edge(2, 3)
-    #
-    # graph.add_edge(1, 2)
-    # graph.add_edge(1, 3)
-    # # graph.add_edge(2, 3)
-    # graph.add_edge(1, 4)
-    # graph.add_edge(2, 4)
-    # graph.add_edge(4, 3)
-
-    return graph
-
-
 def test():
-    # graph = init_graph(UndirectedGraph())
+    # graph = init_graph_one(UndirectedGraph())
     # graph = random_graph(UndirectedGraph())
     # graph = read_astro_dataset('../datasets/CA-AstroPh.txt')
     # graph = read_google_dataset('../datasets/web-Google.txt')
