94cae1db0f113affb5c7c71e097170373ab200af
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2022-05-28 20:59:42 +0300

    reformat structure a bit

--- tasks/degrees.py
+++ tasks/degrees.py
@@ -1,6 +1,3 @@
-import matplotlib.pyplot as plt
-
-
 def node_degrees(graph):
     return dict(map(lambda item: (item[0], len(item[1])), graph.adj.items()))
 
@@ -27,20 +24,3 @@ def average_node_degree(degrees):
         result += current_degree
 
     return result / len(degrees)
-
-
-def probability_function_plot(array):
-    plt.style.use('seaborn-whitegrid')
-
-    figure = plt.figure()
-
-    default_axes = figure.add_subplot(2, 1, 1)
-    loglog_axes = figure.add_subplot(2, 1, 2)
-
-    default_axes.set_title('Default axes')
-    default_axes.set_xlim(0, 1000)
-    default_axes.plot(array)
-    plt.loglog()
-    loglog_axes.plot(array)
-    loglog_axes.set_title('log-log axes')
-    plt.show()
